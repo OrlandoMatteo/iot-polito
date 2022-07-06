4444c567c1263756787508c4f2479d74e9ffbc4e
Matteo Orlando <matteo.orlando.1993@gmail.com>
2022-07-06 12:14:52 +0200

    edit config

--- astro.config.mjs
+++ astro.config.mjs
@@ -10,6 +10,6 @@ export default defineConfig({
 		// Enable React for the Algolia search component.
 		react(),
 	],
-	site:'https://orlandomatteo.github.io/iot-polito/',
+	site:'https://orlandomatteo.github.io/',
 	base: 'iot-polito',
 });
